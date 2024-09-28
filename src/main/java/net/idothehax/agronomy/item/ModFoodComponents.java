a3d7a30d15340439077aa45a189fa6161b0558ca
IDoTheHax <hax@idothehax.com>
2024-09-28 16:39:29 +0200

    Added Food Components

---
+++ src/main/java/net/idothehax/agronomy/item/ModFoodComponents.java
@@ -0,0 +1,69 @@
+package net.idothehax.agronomy.item;
+
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.component.type.FoodComponents;
+
+public class ModFoodComponents {
+    public static final FoodComponent SWIFT = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent PENTLAND_JAVELIN = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent ROCKET = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent CASABLANCA = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent ARRAN_PILOT = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent DUKE_OF_YORK = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent RED_DUKE_OF_YORK = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent EPICURE = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent FOREMOST = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent HOME_GUARD = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent LADY_CHRISTL = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent ORLA = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent PREMIERE = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent PURITAN = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent ULSTER_PRINCE = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent JERSEY_ROYAL = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent MARFONA = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+
+    public static final FoodComponent MARIS_PEER = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent CHARLOTTE = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent NICOLA = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent ANYA = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent BONNIE = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent BRITISH_QUEEN = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent CATRIONA = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent DUNLUCE = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent ESTIMA = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent KESTREL = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent NADINE = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent RATTE = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent WILJA = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+
+    public static final FoodComponent KING_EDWARD = new FoodComponent.Builder().nutrition(5).saturationModifier(0.6f).build();
+    public static final FoodComponent MARIS_PIPER = new FoodComponent.Builder().nutrition(5).saturationModifier(0.6f).build();
+    public static final FoodComponent PICASSO = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent DESIREE = new FoodComponent.Builder().nutrition(5).saturationModifier(0.6f).build();
+    public static final FoodComponent PINK_FIR_APPLE = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent SARPO_MIRA = new FoodComponent.Builder().nutrition(5).saturationModifier(0.6f).build();
+    public static final FoodComponent CARA = new FoodComponent.Builder().nutrition(5).saturationModifier(0.6f).build();
+    public static final FoodComponent MAJESTIC = new FoodComponent.Builder().nutrition(5).saturationModifier(0.6f).build();
+    public static final FoodComponent RECORD = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent ROMANO = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent ROOSTER = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent SANTE = new FoodComponent.Builder().nutrition(5).saturationModifier(0.6f).build();
+    public static final FoodComponent SAXON = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent VALOR = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+
+
+    public static final FoodComponent ELBA = new FoodComponent.Builder().nutrition(5).saturationModifier(0.6f).build();
+    public static final FoodComponent KENNEBEC = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent RED_GOLD = new FoodComponent.Builder().nutrition(5).saturationModifier(0.6f).build();
+    public static final FoodComponent MASQUERADE = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent PURPLE_MAJESTY = new FoodComponent.Builder().nutrition(5).saturationModifier(0.6f).build();
+    public static final FoodComponent RUSSIAN_BANANA_FINGERLING = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent RED_NORLAND = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent KIFLI = new FoodComponent.Builder().nutrition(5).saturationModifier(0.6f).build();
+    public static final FoodComponent LADY_BALFOUR = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent VIVALDI = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent ACCORD = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent CONGO = new FoodComponent.Builder().nutrition(4).saturationModifier(0.5f).build();
+    public static final FoodComponent MAYAN_GOLD = new FoodComponent.Builder().nutrition(5).saturationModifier(0.6f).build();
+
+}
--- src/main/java/net/idothehax/agronomy/item/ModItems.java
+++ src/main/java/net/idothehax/agronomy/item/ModItems.java
@@ -8,73 +8,74 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
+import static net.idothehax.agronomy.item.ModFoodComponents.*;
+
 public class ModItems {
 
     // First Earlies
-    public static final Item SWIFT_SEED = new BaseSeedItem(ModBlocks.SWIFT_POTATOES, new Item.Settings(), ModPotatoVariants.SWIFT);
-    public static final Item PENTLAND_JAVELIN_SEED = new BaseSeedItem(ModBlocks.PENTLAND_JAVELIN_POTATOES, new Item.Settings(), ModPotatoVariants.PENTLAND_JAVELIN);
-    public static final Item ROCKET_SEED = new BaseSeedItem(ModBlocks.ROCKET_POTATOES,new Item.Settings(), ModPotatoVariants.ROCKET);
-    public static final Item CASABLANCA_SEED = new BaseSeedItem(ModBlocks.CASABLANCA_POTATOES,new Item.Settings(), ModPotatoVariants.CASABLANCA);
-    public static final Item ARRAN_PILOT_SEED = new BaseSeedItem(ModBlocks.ARRAN_PILOT_POTATOES,new Item.Settings(), ModPotatoVariants.ARRAN_PILOT);
-    public static final Item DUKE_OF_YORK_SEED = new BaseSeedItem(ModBlocks.DUKE_OF_YORK_POTATOES,new Item.Settings(), ModPotatoVariants.DUKE_OF_YORK);
-    public static final Item RED_DUKE_OF_YORK_SEED = new BaseSeedItem(ModBlocks.RED_DUKE_OF_YORK_POTATOES,new Item.Settings(), ModPotatoVariants.RED_DUKE_OF_YORK);
-    public static final Item EPICURE_SEED = new BaseSeedItem(ModBlocks.FOREMOST_POTATOES,new Item.Settings(), ModPotatoVariants.EPICURE);
-    public static final Item FOREMOST_SEED = new BaseSeedItem(ModBlocks.FOREMOST_POTATOES, new Item.Settings(), ModPotatoVariants.FOREMOST);
-    public static final Item HOME_GUARD_SEED = new BaseSeedItem(ModBlocks.HOME_GUARD_POTATOES, new Item.Settings(), ModPotatoVariants.HOME_GUARD);
-    public static final Item LADY_CHRISTL_SEED = new BaseSeedItem(ModBlocks.LADY_CHRISTL_POTATOES, new Item.Settings(), ModPotatoVariants.LADY_CHRISTL);
-    public static final Item ORLA_SEED = new BaseSeedItem(ModBlocks.ORLA_POTATOES, new Item.Settings(), ModPotatoVariants.ORLA);
-    public static final Item PREMIERE_SEED = new BaseSeedItem(ModBlocks.PREMIERE_POTATOES, new Item.Settings(), ModPotatoVariants.PREMIERE);
-    public static final Item PURITAN_SEED = new BaseSeedItem(ModBlocks.PURITAN_POTATOES, new Item.Settings(), ModPotatoVariants.PURITAN);
-    public static final Item ULSTER_PRINCE_SEED = new BaseSeedItem(ModBlocks.ULSTER_PRINCE_POTATOES, new Item.Settings(), ModPotatoVariants.ULSTER_PRINCE);
-    public static final Item JERSEY_ROYAL_SEED = new BaseSeedItem(ModBlocks.JERSEY_ROYAL_POTATOES, new Item.Settings(), ModPotatoVariants.JERSEY_ROYAL);
-    public static final Item MARFONA_SEED = new BaseSeedItem(ModBlocks.MARFONA_POTATOES, new Item.Settings(), ModPotatoVariants.MARFONA);
+    public static final Item SWIFT_SEED = new BaseSeedItem(ModBlocks.SWIFT_POTATOES, new Item.Settings().food(SWIFT), ModPotatoVariants.SWIFT);
+    public static final Item PENTLAND_JAVELIN_SEED = new BaseSeedItem(ModBlocks.PENTLAND_JAVELIN_POTATOES, new Item.Settings().food(PENTLAND_JAVELIN), ModPotatoVariants.PENTLAND_JAVELIN);
+    public static final Item ROCKET_SEED = new BaseSeedItem(ModBlocks.ROCKET_POTATOES, new Item.Settings().food(ROCKET), ModPotatoVariants.ROCKET);
+    public static final Item CASABLANCA_SEED = new BaseSeedItem(ModBlocks.CASABLANCA_POTATOES, new Item.Settings().food(CASABLANCA), ModPotatoVariants.CASABLANCA);
+    public static final Item ARRAN_PILOT_SEED = new BaseSeedItem(ModBlocks.ARRAN_PILOT_POTATOES, new Item.Settings().food(ARRAN_PILOT), ModPotatoVariants.ARRAN_PILOT);
+    public static final Item DUKE_OF_YORK_SEED = new BaseSeedItem(ModBlocks.DUKE_OF_YORK_POTATOES, new Item.Settings().food(DUKE_OF_YORK), ModPotatoVariants.DUKE_OF_YORK);
+    public static final Item RED_DUKE_OF_YORK_SEED = new BaseSeedItem(ModBlocks.RED_DUKE_OF_YORK_POTATOES, new Item.Settings().food(RED_DUKE_OF_YORK), ModPotatoVariants.RED_DUKE_OF_YORK);
+    public static final Item EPICURE_SEED = new BaseSeedItem(ModBlocks.EPICURE_POTATOES, new Item.Settings().food(EPICURE), ModPotatoVariants.EPICURE);
+    public static final Item FOREMOST_SEED = new BaseSeedItem(ModBlocks.FOREMOST_POTATOES, new Item.Settings().food(FOREMOST), ModPotatoVariants.FOREMOST);
+    public static final Item HOME_GUARD_SEED = new BaseSeedItem(ModBlocks.HOME_GUARD_POTATOES, new Item.Settings().food(HOME_GUARD), ModPotatoVariants.HOME_GUARD);
+    public static final Item LADY_CHRISTL_SEED = new BaseSeedItem(ModBlocks.LADY_CHRISTL_POTATOES, new Item.Settings().food(LADY_CHRISTL), ModPotatoVariants.LADY_CHRISTL);
+    public static final Item ORLA_SEED = new BaseSeedItem(ModBlocks.ORLA_POTATOES, new Item.Settings().food(ORLA), ModPotatoVariants.ORLA);
+    public static final Item PREMIERE_SEED = new BaseSeedItem(ModBlocks.PREMIERE_POTATOES, new Item.Settings().food(PREMIERE), ModPotatoVariants.PREMIERE);
+    public static final Item PURITAN_SEED = new BaseSeedItem(ModBlocks.PURITAN_POTATOES, new Item.Settings().food(PURITAN), ModPotatoVariants.PURITAN);
+    public static final Item ULSTER_PRINCE_SEED = new BaseSeedItem(ModBlocks.ULSTER_PRINCE_POTATOES, new Item.Settings().food(ULSTER_PRINCE), ModPotatoVariants.ULSTER_PRINCE);
+    public static final Item JERSEY_ROYAL_SEED = new BaseSeedItem(ModBlocks.JERSEY_ROYAL_POTATOES, new Item.Settings().food(JERSEY_ROYAL), ModPotatoVariants.JERSEY_ROYAL);
+    public static final Item MARFONA_SEED = new BaseSeedItem(ModBlocks.MARFONA_POTATOES, new Item.Settings().food(MARFONA), ModPotatoVariants.MARFONA);
 
     // Second Earlies
-    public static final Item MARIS_PEER_SEED = new BaseSeedItem(ModBlocks.MARIS_PEER_POTATOES, new Item.Settings(), ModPotatoVariants.MARIS_PEER);
-    public static final Item CHARLOTTE_SEED = new BaseSeedItem(ModBlocks.CHARLOTTE_POTATOES, new Item.Settings(), ModPotatoVariants.CHARLOTTE);
-    public static final Item NICOLA_SEED = new BaseSeedItem(ModBlocks.NICOLA_POTATOES, new Item.Settings(), ModPotatoVariants.NICOLA);
-    public static final Item ANYA_SEED = new BaseSeedItem(ModBlocks.ANYA_POTATOES, new Item.Settings(), ModPotatoVariants.ANYA);
-    public static final Item BONNIE_SEED = new BaseSeedItem(ModBlocks.BONNIE_POTATOES, new Item.Settings(), ModPotatoVariants.BONNIE);
-    public static final Item BRITISH_QUEEN_SEED = new BaseSeedItem(ModBlocks.BRITISH_QUEEN_POTATOES, new Item.Settings(), ModPotatoVariants.BRITISH_QUEEN);
-    public static final Item CATRIONA_SEED = new BaseSeedItem(ModBlocks.CATRIONA_POTATOES, new Item.Settings(), ModPotatoVariants.CATRIONA);
-    public static final Item DUNLUCE_SEED = new BaseSeedItem(ModBlocks.DUNLUCE_POTATOES, new Item.Settings(), ModPotatoVariants.DUNLUCE);
-    public static final Item ESTIMA_SEED = new BaseSeedItem(ModBlocks.ESTIMA_POTATOES, new Item.Settings(), ModPotatoVariants.ESTIMA);
-    public static final Item KESTREL_SEED = new BaseSeedItem(ModBlocks.KESTREL_POTATOES, new Item.Settings(), ModPotatoVariants.KESTREL);
-    public static final Item NADINE_SEED = new BaseSeedItem(ModBlocks.NADINE_POTATOES, new Item.Settings(), ModPotatoVariants.NADINE);
-    public static final Item RATTE_SEED = new BaseSeedItem(ModBlocks.RATTE_POTATOES, new Item.Settings(), ModPotatoVariants.RATTE);
-    public static final Item WILJA_SEED = new BaseSeedItem(ModBlocks.WILJA_POTATOES, new Item.Settings(), ModPotatoVariants.WILJA);
+    public static final Item MARIS_PEER_SEED = new BaseSeedItem(ModBlocks.MARIS_PEER_POTATOES, new Item.Settings().food(MARIS_PEER), ModPotatoVariants.MARIS_PEER);
+    public static final Item CHARLOTTE_SEED = new BaseSeedItem(ModBlocks.CHARLOTTE_POTATOES, new Item.Settings().food(CHARLOTTE), ModPotatoVariants.CHARLOTTE);
+    public static final Item NICOLA_SEED = new BaseSeedItem(ModBlocks.NICOLA_POTATOES, new Item.Settings().food(NICOLA), ModPotatoVariants.NICOLA);
+    public static final Item ANYA_SEED = new BaseSeedItem(ModBlocks.ANYA_POTATOES, new Item.Settings().food(ANYA), ModPotatoVariants.ANYA);
+    public static final Item BONNIE_SEED = new BaseSeedItem(ModBlocks.BONNIE_POTATOES, new Item.Settings().food(BONNIE), ModPotatoVariants.BONNIE);
+    public static final Item BRITISH_QUEEN_SEED = new BaseSeedItem(ModBlocks.BRITISH_QUEEN_POTATOES, new Item.Settings().food(BRITISH_QUEEN), ModPotatoVariants.BRITISH_QUEEN);
+    public static final Item CATRIONA_SEED = new BaseSeedItem(ModBlocks.CATRIONA_POTATOES, new Item.Settings().food(CATRIONA), ModPotatoVariants.CATRIONA);
+    public static final Item DUNLUCE_SEED = new BaseSeedItem(ModBlocks.DUNLUCE_POTATOES, new Item.Settings().food(DUNLUCE), ModPotatoVariants.DUNLUCE);
+    public static final Item ESTIMA_SEED = new BaseSeedItem(ModBlocks.ESTIMA_POTATOES, new Item.Settings().food(ESTIMA), ModPotatoVariants.ESTIMA);
+    public static final Item KESTREL_SEED = new BaseSeedItem(ModBlocks.KESTREL_POTATOES, new Item.Settings().food(KESTREL), ModPotatoVariants.KESTREL);
+    public static final Item NADINE_SEED = new BaseSeedItem(ModBlocks.NADINE_POTATOES, new Item.Settings().food(NADINE), ModPotatoVariants.NADINE);
+    public static final Item RATTE_SEED = new BaseSeedItem(ModBlocks.RATTE_POTATOES, new Item.Settings().food(RATTE), ModPotatoVariants.RATTE);
+    public static final Item WILJA_SEED = new BaseSeedItem(ModBlocks.WILJA_POTATOES, new Item.Settings().food(WILJA), ModPotatoVariants.WILJA);
 
     // Maincrop
-    public static final Item KING_EDWARD_SEED = new BaseSeedItem(ModBlocks.KING_EDWARD_POTATOES, new Item.Settings(), ModPotatoVariants.KING_EDWARD);
-    public static final Item MARIS_PIPER_SEED = new BaseSeedItem(ModBlocks.MARIS_PIPER_POTATOES, new Item.Settings(), ModPotatoVariants.MARIS_PIPER);
-    public static final Item PICASSO_SEED = new BaseSeedItem(ModBlocks.PICASSO_POTATOES, new Item.Settings(), ModPotatoVariants.PICASSO);
-    public static final Item DESIREE_SEED = new BaseSeedItem(ModBlocks.DESIREE_POTATOES, new Item.Settings(), ModPotatoVariants.DESIREE);
-    public static final Item PINK_FIR_APPLE_SEED = new BaseSeedItem(ModBlocks.PINK_FIR_APPLE_POTATOES, new Item.Settings(), ModPotatoVariants.PINK_FIR_APPLE);
-    public static final Item SARPO_MIRA_SEED = new BaseSeedItem(ModBlocks.SARPO_MIRA_POTATOES, new Item.Settings(), ModPotatoVariants.SARPO_MIRA);
-    public static final Item CARA_SEED = new BaseSeedItem(ModBlocks.CARA_POTATOES, new Item.Settings(), ModPotatoVariants.CARA);
-    public static final Item MAJESTIC_SEED = new BaseSeedItem(ModBlocks.MAJESTIC_POTATOES, new Item.Settings(), ModPotatoVariants.MAJESTIC);
-    public static final Item RECORD_SEED = new BaseSeedItem(ModBlocks.RECORD_POTATOES, new Item.Settings(), ModPotatoVariants.RECORD);
-    public static final Item ROMANO_SEED = new BaseSeedItem(ModBlocks.ROMANO_POTATOES, new Item.Settings(), ModPotatoVariants.ROMANO);
-    public static final Item ROOSTER_SEED = new BaseSeedItem(ModBlocks.ROOSTER_POTATOES, new Item.Settings(), ModPotatoVariants.ROOSTER);
-    public static final Item SANTE_SEED = new BaseSeedItem(ModBlocks.SANTE_POTATOES, new Item.Settings(), ModPotatoVariants.SANTE);
-    public static final Item SAXON_SEED = new BaseSeedItem(ModBlocks.SAXON_POTATOES, new Item.Settings(), ModPotatoVariants.SAXON);
-    public static final Item VALOR_SEED = new BaseSeedItem(ModBlocks.VALOR_POTATOES, new Item.Settings(), ModPotatoVariants.VALOR);
-
+    public static final Item KING_EDWARD_SEED = new BaseSeedItem(ModBlocks.KING_EDWARD_POTATOES, new Item.Settings().food(KING_EDWARD), ModPotatoVariants.KING_EDWARD);
+    public static final Item MARIS_PIPER_SEED = new BaseSeedItem(ModBlocks.MARIS_PIPER_POTATOES, new Item.Settings().food(MARIS_PIPER), ModPotatoVariants.MARIS_PIPER);
+    public static final Item PICASSO_SEED = new BaseSeedItem(ModBlocks.PICASSO_POTATOES, new Item.Settings().food(PICASSO), ModPotatoVariants.PICASSO);
+    public static final Item DESIREE_SEED = new BaseSeedItem(ModBlocks.DESIREE_POTATOES, new Item.Settings().food(DESIREE), ModPotatoVariants.DESIREE);
+    public static final Item PINK_FIR_APPLE_SEED = new BaseSeedItem(ModBlocks.PINK_FIR_APPLE_POTATOES, new Item.Settings().food(PINK_FIR_APPLE), ModPotatoVariants.PINK_FIR_APPLE);
+    public static final Item SARPO_MIRA_SEED = new BaseSeedItem(ModBlocks.SARPO_MIRA_POTATOES, new Item.Settings().food(SARPO_MIRA), ModPotatoVariants.SARPO_MIRA);
+    public static final Item CARA_SEED = new BaseSeedItem(ModBlocks.CARA_POTATOES, new Item.Settings().food(CARA), ModPotatoVariants.CARA);
+    public static final Item MAJESTIC_SEED = new BaseSeedItem(ModBlocks.MAJESTIC_POTATOES, new Item.Settings().food(MAJESTIC), ModPotatoVariants.MAJESTIC);
+    public static final Item RECORD_SEED = new BaseSeedItem(ModBlocks.RECORD_POTATOES, new Item.Settings().food(RECORD), ModPotatoVariants.RECORD);
+    public static final Item ROMANO_SEED = new BaseSeedItem(ModBlocks.ROMANO_POTATOES, new Item.Settings().food(ROMANO), ModPotatoVariants.ROMANO);
+    public static final Item ROOSTER_SEED = new BaseSeedItem(ModBlocks.ROOSTER_POTATOES, new Item.Settings().food(ROOSTER), ModPotatoVariants.ROOSTER);
+    public static final Item SANTE_SEED = new BaseSeedItem(ModBlocks.SANTE_POTATOES, new Item.Settings().food(SANTE), ModPotatoVariants.SANTE);
+    public static final Item SAXON_SEED = new BaseSeedItem(ModBlocks.SAXON_POTATOES, new Item.Settings().food(SAXON), ModPotatoVariants.SAXON);
+    public static final Item VALOR_SEED = new BaseSeedItem(ModBlocks.VALOR_POTATOES, new Item.Settings().food(VALOR), ModPotatoVariants.VALOR);
 
     // Specialty Varieties
-    public static final Item ELBA_SEED = new BaseSeedItem(ModBlocks.ELBA_POTATOES, new Item.Settings(), ModPotatoVariants.ELBA);
-    public static final Item KENNEBEC_SEED = new BaseSeedItem(ModBlocks.KENNEBEC_POTATOES, new Item.Settings(), ModPotatoVariants.KENNEBEC);
-    public static final Item RED_GOLD_SEED = new BaseSeedItem(ModBlocks.RED_GOLD_POTATOES, new Item.Settings(), ModPotatoVariants.RED_GOLD);
-    public static final Item MASQUERADE_SEED = new BaseSeedItem(ModBlocks.MASQUERADE_POTATOES, new Item.Settings(), ModPotatoVariants.MASQUERADE);
-    public static final Item PURPLE_MAJESTY_SEED = new BaseSeedItem(ModBlocks.PURPLE_MAJESTY_POTATOES, new Item.Settings(), ModPotatoVariants.PURPLE_MAJESTY);
-    public static final Item RUSSIAN_BANANA_FINGERLING_SEED = new BaseSeedItem(ModBlocks.RUSSIAN_BANANA_FINGERLING_POTATOES, new Item.Settings(), ModPotatoVariants.RUSSIAN_BANANA_FINGERLING);
-    public static final Item RED_NORLAND_SEED = new BaseSeedItem(ModBlocks.RED_NORLAND_POTATOES, new Item.Settings(), ModPotatoVariants.RED_NORLAND);
-    public static final Item KIFLI_SEED = new BaseSeedItem(ModBlocks.KIFLI_POTATOES, new Item.Settings(), ModPotatoVariants.KIFLI);
-    public static final Item LADY_BALFOUR_SEED = new BaseSeedItem(ModBlocks.LADY_BALFOUR_POTATOES, new Item.Settings(), ModPotatoVariants.LADY_BALFOUR);
-    public static final Item VIVALDI_SEED = new BaseSeedItem(ModBlocks.VIVALDI_POTATOES, new Item.Settings(), ModPotatoVariants.VIVALDI);
-    public static final Item ACCORD_SEED = new BaseSeedItem(ModBlocks.ACCORD_POTATOES, new Item.Settings(), ModPotatoVariants.ACCORD);
-    public static final Item CONGO_SEED = new BaseSeedItem(ModBlocks.CONGO_POTATOES, new Item.Settings(), ModPotatoVariants.CONGO);
-    public static final Item MAYAN_GOLD_SEED = new BaseSeedItem(ModBlocks.MAYAN_GOLD_POTATOES, new Item.Settings(), ModPotatoVariants.MAYAN_GOLD);
+    public static final Item ELBA_SEED = new BaseSeedItem(ModBlocks.ELBA_POTATOES, new Item.Settings().food(ELBA), ModPotatoVariants.ELBA);
+    public static final Item KENNEBEC_SEED = new BaseSeedItem(ModBlocks.KENNEBEC_POTATOES, new Item.Settings().food(KENNEBEC), ModPotatoVariants.KENNEBEC);
+    public static final Item RED_GOLD_SEED = new BaseSeedItem(ModBlocks.RED_GOLD_POTATOES, new Item.Settings().food(RED_GOLD), ModPotatoVariants.RED_GOLD);
+    public static final Item MASQUERADE_SEED = new BaseSeedItem(ModBlocks.MASQUERADE_POTATOES, new Item.Settings().food(MASQUERADE), ModPotatoVariants.MASQUERADE);
+    public static final Item PURPLE_MAJESTY_SEED = new BaseSeedItem(ModBlocks.PURPLE_MAJESTY_POTATOES, new Item.Settings().food(PURPLE_MAJESTY), ModPotatoVariants.PURPLE_MAJESTY);
+    public static final Item RUSSIAN_BANANA_FINGERLING_SEED = new BaseSeedItem(ModBlocks.RUSSIAN_BANANA_FINGERLING_POTATOES, new Item.Settings().food(RUSSIAN_BANANA_FINGERLING), ModPotatoVariants.RUSSIAN_BANANA_FINGERLING);
+    public static final Item RED_NORLAND_SEED = new BaseSeedItem(ModBlocks.RED_NORLAND_POTATOES, new Item.Settings().food(RED_NORLAND), ModPotatoVariants.RED_NORLAND);
+    public static final Item KIFLI_SEED = new BaseSeedItem(ModBlocks.KIFLI_POTATOES, new Item.Settings().food(KIFLI), ModPotatoVariants.KIFLI);
+    public static final Item LADY_BALFOUR_SEED = new BaseSeedItem(ModBlocks.LADY_BALFOUR_POTATOES, new Item.Settings().food(LADY_BALFOUR), ModPotatoVariants.LADY_BALFOUR);
+    public static final Item VIVALDI_SEED = new BaseSeedItem(ModBlocks.VIVALDI_POTATOES, new Item.Settings().food(VIVALDI), ModPotatoVariants.VIVALDI);
+    public static final Item ACCORD_SEED = new BaseSeedItem(ModBlocks.ACCORD_POTATOES, new Item.Settings().food(ACCORD), ModPotatoVariants.ACCORD);
+    public static final Item CONGO_SEED = new BaseSeedItem(ModBlocks.CONGO_POTATOES, new Item.Settings().food(CONGO), ModPotatoVariants.CONGO);
+    public static final Item MAYAN_GOLD_SEED = new BaseSeedItem(ModBlocks.MAYAN_GOLD_POTATOES, new Item.Settings().food(MAYAN_GOLD), ModPotatoVariants.MAYAN_GOLD);
 
     public static void registerItems() {
         // Register First Earlies
